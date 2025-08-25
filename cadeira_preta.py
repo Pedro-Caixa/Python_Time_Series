f76b754b4c400d484a88969cfe102a90e5a7bd79
Caxseii <caxseiiiia@gmail.com>
2025-08-25 20:54:02 -0300

    Resposta header a doc string

---
+++ cadeira_preta.py
@@ -0,0 +1,7 @@
+// O que é um cabeçalho do arquivo, e qual é a diferença de uma doc string
+
+print("Um Header File é um arquivo onde são colocadas funções e atributos separados")
+
+def cadeira()
+ '''Esse é um doc string de uma linha só'''
+    print("Esse é um docstring")
