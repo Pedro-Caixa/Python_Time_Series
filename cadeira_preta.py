9c10ea085ea7f12a4b4f326f77cf3a26a58a1fee
Fabiano Dicheti <117326271+FabianoDicheti@users.noreply.github.com>
2025-08-26 14:13:53 -0300

    Update cadeira_preta.py

--- cadeira_preta.py
+++ cadeira_preta.py
@@ -5,3 +5,29 @@ print("Um Header File é um arquivo onde são colocadas funções e atributos se
 def cadeira()
  '''Esse é um doc string de uma linha só'''
     print("Esse é um docstring")
+
+# coloque o header deste arquivo
+
+
+# implemente as funções abaixo e coloque as docstrings
+
+
+# Segue atividade, implemente os métodos abaixo:
+
+def maximo(nums):
+    """há um padrão pra docstring
+    oque faz
+    oque recebe
+    oque retorna"""
+    # TODO: percorra a lista guardando o maior atual
+    ...
+
+def e_par(n: int) -> bool:
+    """ ... """
+    # TODO: retorne se n é par
+    ...
+  
+def fatorial(n: int) -> int:
+    """   ...  """
+    # TODO: implemente de forma iterativa (sem recursão)
+    ...
